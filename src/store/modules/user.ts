e18b39e9943bfea1ca8fb69df5b3cf2332629f96
java20030215 <1349947654@qq.com>
2024-05-04 01:05:28 +0800

    re pinia store

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -3,7 +3,6 @@ import { defineStore } from 'pinia';
 //引入接口
 import { reqLogin, reqUserInfo, reqLogout } from '@/api/user';
 import type { loginFormData, loginResponseData, userInfoReponseData } from '@/api/user/type';
-import type { UserState } from './types/type';
 //引入操作本地存储的工具方法
 import { SET_TOKEN, GET_TOKEN, REMOVE_TOKEN } from '@/utils/token';
 //引入路由(常量路由)
@@ -12,6 +11,7 @@ import { constantRoute, asnycRoute, anyRoute } from '@/router/routes';
 //引入深拷贝方法
 import cloneDeep from 'lodash/cloneDeep';
 import router from '@/router';
+import { ref } from 'vue';
 //用于过滤当前用户需要展示的异步路由
 function filterAsyncRoute(asnycRoute: any, routes: any) {
   return asnycRoute.filter((item: any) => {
@@ -25,78 +25,156 @@ function filterAsyncRoute(asnycRoute: any, routes: any) {
   });
 }
 
-//创建用户小仓库
-const useUserStore = defineStore('User', {
-  //小仓库存储数据地方
-  state: (): UserState => {
-    return {
-      token: GET_TOKEN(), //用户唯一标识token
-      menuRoutes: constantRoute, //仓库存储生成菜单需要数组(路由)
-      username: '',
-      avatar: '',
-      //存储当前用户是否包含某一个按钮
-      buttons: []
-    };
-  },
-  //异步|逻辑的地方
-  actions: {
-    //用户登录的方法
-    async userLogin(data: loginFormData) {
-      //登录请求
-      const result: loginResponseData = await reqLogin(data);
-      //登录请求:成功200->token
-      //登录请求:失败201->登录失败错误的信息
-      if (result.code == 200) {
-        //pinia仓库存储一下token
-        //由于pinia|vuex存储数据其实利用js对象
-        this.token = result.data as string;
-        //本地存储持久化存储一份
-        SET_TOKEN(result.data as string);
-        //能保证当前async函数返回一个成功的promise
-        return 'ok';
-      } else {
-        return Promise.reject(new Error(result.data));
-      }
-    },
-    //获取用户信息方法
-    async userInfo() {
-      //获取用户信息进行存储仓库当中[用户头像、名字]
-      const result: userInfoReponseData = await reqUserInfo();
-      //如果获取用户信息成功，存储一下用户信息
-      if (result.code == 200) {
-        this.username = result.data.name;
-        this.avatar = result.data.avatar;
-        this.buttons = result.data.buttons;
-        //计算当前用户需要展示的异步路由
-        const userAsyncRoute = filterAsyncRoute(cloneDeep(asnycRoute), result.data.routes);
-        //菜单需要的数据整理完毕
-        this.menuRoutes = [...constantRoute, ...userAsyncRoute, anyRoute];
-        //目前路由器管理的只有常量路由:用户计算完毕异步路由、任意路由动态追加
-        [...userAsyncRoute, anyRoute].forEach((route: any) => {
-          router.addRoute(route);
-        });
-        return 'ok';
-      } else {
-        return Promise.reject(new Error(result.message));
-      }
-    },
-    //退出登录
-    async userLogout() {
-      //退出登录请求
-      const result: any = await reqLogout();
-      if (result.code == 200) {
-        //目前没有mock接口:退出登录接口(通知服务器本地用户唯一标识失效)
-        this.token = '';
-        this.username = '';
-        this.avatar = '';
-        REMOVE_TOKEN();
-        return 'ok';
-      } else {
-        return Promise.reject(new Error(result.message));
-      }
+// //创建用户小仓库
+// const useUserStore = defineStore('User',
+// {
+//   //小仓库存储数据地方
+//   state: (): UserState => {
+//     return {
+//       token: GET_TOKEN(), //用户唯一标识token
+//       menuRoutes: constantRoute, //仓库存储生成菜单需要数组(路由)
+//       username: '',
+//       avatar: '',
+//       //存储当前用户是否包含某一个按钮
+//       buttons: []
+//     };
+//   },
+//   //异步|逻辑的地方
+//   actions: {
+//     //用户登录的方法
+//     async userLogin(data: loginFormData) {
+//       //登录请求
+//       const result: loginResponseData = await reqLogin(data);
+//       //登录请求:成功200->token
+//       //登录请求:失败201->登录失败错误的信息
+//       if (result.code == 200) {
+//         //pinia仓库存储一下token
+//         //由于pinia|vuex存储数据其实利用js对象
+//         token = result.data as string;
+//         //本地存储持久化存储一份
+//         SET_TOKEN(result.data as string);
+//         //能保证当前async函数返回一个成功的promise
+//         return 'ok';
+//       } else {
+//         return Promise.reject(new Error(result.data));
+//       }
+//     },
+//     //获取用户信息方法
+//     async userInfo() {
+//       //获取用户信息进行存储仓库当中[用户头像、名字]
+//       const result: userInfoReponseData = await reqUserInfo();
+//       //如果获取用户信息成功，存储一下用户信息
+//       if (result.code == 200) {
+//         username = result.data.name;
+//         avatar = result.data.avatar;
+//         buttons = result.data.buttons;
+//         //计算当前用户需要展示的异步路由
+//         const userAsyncRoute = filterAsyncRoute(cloneDeep(asnycRoute), result.data.routes);
+//         //菜单需要的数据整理完毕
+//         menuRoutes = [...constantRoute, ...userAsyncRoute, anyRoute];
+//         //目前路由器管理的只有常量路由:用户计算完毕异步路由、任意路由动态追加
+//         [...userAsyncRoute, anyRoute].forEach((route: any) => {
+//           router.addRoute(route);
+//         });
+//         return 'ok';
+//       } else {
+//         return Promise.reject(new Error(result.message));
+//       }
+//     },
+//     //退出登录
+//     async userLogout() {
+//       //退出登录请求
+//       const result: any = await reqLogout();
+//       if (result.code == 200) {
+//         //目前没有mock接口:退出登录接口(通知服务器本地用户唯一标识失效)
+//         token = '';
+//         username = '';
+//         avatar = '';
+//         REMOVE_TOKEN();
+//         return 'ok';
+//       } else {
+//         return Promise.reject(new Error(result.message));
+//       }
+//     }
+//   },
+//   getters: {}
+// });
+
+const useUserStore = defineStore('User', () => {
+  const token = ref<string | null>(GET_TOKEN());
+  const menuRoutes = ref<any>(constantRoute);
+  const username = ref<string>('');
+  const avatar = ref<string>('');
+  const buttons = ref<any[]>([]);
+
+  //用户登录的方法
+  const userLogin = async (data: loginFormData) => {
+    //登录请求
+    const result: loginResponseData = await reqLogin(data);
+    //登录请求:成功200->token
+    //登录请求:失败201->登录失败错误的信息
+    if (result.code == 200) {
+      //pinia仓库存储一下token
+      //由于pinia|vuex存储数据其实利用js对象
+      token.value = result.data as string;
+      //本地存储持久化存储一份
+      SET_TOKEN(result.data as string);
+      //能保证当前async函数返回一个成功的promise
+      return 'ok';
+    } else {
+      return Promise.reject(new Error(result.data));
     }
-  },
-  getters: {}
+  };
+
+  //获取用户信息方法
+  const userInfo = async () => {
+    //获取用户信息进行存储仓库当中[用户头像、名字]
+    const result: userInfoReponseData = await reqUserInfo();
+    //如果获取用户信息成功，存储一下用户信息
+    if (result.code == 200) {
+      username.value = result.data.name;
+      avatar.value = result.data.avatar;
+      buttons.value = result.data.buttons;
+      //计算当前用户需要展示的异步路由
+      const userAsyncRoute = filterAsyncRoute(cloneDeep(asnycRoute), result.data.routes);
+      //菜单需要的数据整理完毕
+      menuRoutes.value = [...constantRoute, ...userAsyncRoute, anyRoute];
+      //目前路由器管理的只有常量路由:用户计算完毕异步路由、任意路由动态追加
+      [...userAsyncRoute, anyRoute].forEach((route: any) => {
+        router.addRoute(route);
+      });
+      return 'ok';
+    } else {
+      return Promise.reject(new Error(result.message));
+    }
+  };
+
+  //退出登录
+  const userLogout = async () => {
+    //退出登录请求
+    const result: any = await reqLogout();
+    if (result.code == 200) {
+      //目前没有mock接口:退出登录接口(通知服务器本地用户唯一标识失效)
+      token.value = '';
+      username.value = '';
+      avatar.value = '';
+      REMOVE_TOKEN();
+      return 'ok';
+    } else {
+      return Promise.reject(new Error(result.message));
+    }
+  };
+  return {
+    token,
+    menuRoutes,
+    username,
+    avatar,
+    buttons,
+
+    userLogin,
+    userInfo,
+    userLogout
+  };
 });
 //对外暴露获取小仓库方法
 export default useUserStore;
